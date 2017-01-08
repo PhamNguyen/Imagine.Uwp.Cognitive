3588ff912ca0f964cb8ff5925632633e61c9a142
PhamNguyen <pham.nguyen@hotmail.com>
2017-01-08 18:29:21 +0700

    Imagine.Uwp.Cognitive

---
+++ Imagine.Uwp.Cognitive/Authentication.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Imagine.Uwp.Cognitive
+{
+    /// <summary>
+    /// This class demonstrates how to get a valid O-auth token
+    /// </summary>
+    public class Authentication
+    {
+        public static readonly string AccessUri = "https://api.cognitive.microsoft.com/sts/v1.0/issueToken";
+        private static string apiKey;
+        private string accessToken;
+        private Timer accessTokenRenewer;
+
+        //Access token expires every 10 minutes. Renew it every 9 minutes only.
+        private const int RefreshTokenDuration = 9;
+
+        public Authentication(String apiKey, Action callback = null) {
+            Auth(apiKey, callback);
+        }
+
+        public async Task Auth(string apiKey, Action callback = null)
+        {
+            apiKey = apiKey;
+
+            this.accessToken = await HttpPost(AccessUri, apiKey);
+
+            // renew the token every specfied minutes
+            accessTokenRenewer = new Timer(new TimerCallback(OnTokenExpiredCallback),
+                                           this,
+                                           TimeSpan.FromMinutes(RefreshTokenDuration),
+                                           TimeSpan.FromMilliseconds(-1));
+            callback?.Invoke();
+        }
+
+        public string GetAccessToken()
+        {
+            return this.accessToken;
+        }
+
+        private async Task RenewAccessToken()
+        {
+            string newAccessToken = await HttpPost(AccessUri, apiKey);
+            //swap the new token with old one
+            //Note: the swap is thread unsafe
+            this.accessToken = newAccessToken;
+            Debug.WriteLine(string.Format("Renewed token for user: {0} is: {1}",
+                              apiKey,
+                              this.accessToken));
+        }
+
+        private async void OnTokenExpiredCallback(object stateInfo)
+        {
+            try
+            {
+                await RenewAccessToken();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(string.Format("Failed renewing access token. Details: {0}", ex.Message));
+            }
+            finally
+            {
+                try
+                {
+                    accessTokenRenewer.Change(TimeSpan.FromMinutes(RefreshTokenDuration), TimeSpan.FromMilliseconds(-1));
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(string.Format("Failed to reschedule the timer to renew access token. Details: {0}", ex.Message));
+                }
+            }
+        }
+
+        private async Task<string> HttpPost(string accessUri, string apiKey)
+        {
+            // Prepare OAuth request 
+            WebRequest webRequest = WebRequest.Create(accessUri);
+            webRequest.Method = "POST";
+            //webRequest.ContentLength = 0;
+            webRequest.Headers["Ocp-Apim-Subscription-Key"] = apiKey;
+
+            using (WebResponse webResponse = await webRequest.GetResponseAsync())
+            {
+                using (Stream stream = webResponse.GetResponseStream())
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        byte[] waveBytes = null;
+                        int count = 0;
+                        do
+                        {
+                            byte[] buf = new byte[1024];
+                            count = stream.Read(buf, 0, 1024);
+                            ms.Write(buf, 0, count);
+                        } while (stream.CanRead && count > 0);
+
+                        waveBytes = ms.ToArray();
+
+                        return Encoding.UTF8.GetString(waveBytes);
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Generic event args
+    /// </summary>
+    /// <typeparam name="T">Any type T</typeparam>
+    public class GenericEventArgs<T> : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericEventArgs{T}" /> class.
+        /// </summary>
+        /// <param name="eventData">The event data.</param>
+        public GenericEventArgs(T eventData)
+        {
+            this.EventData = eventData;
+        }
+
+        /// <summary>
+        /// Gets the event data.
+        /// </summary>
+        public T EventData { get; private set; }
+    }
+
+    /// <summary>
+    /// Gender of the voice.
+    /// </summary>
+    public enum Gender
+    {
+        Female,
+        Male
+    }
+
+    /// <summary>
+    /// Voice output formats.
+    /// </summary>
+    public enum AudioOutputFormat
+    {
+        /// <summary>
+        /// raw-8khz-8bit-mono-mulaw request output audio format type.
+        /// </summary>
+        Raw8Khz8BitMonoMULaw,
+        /// <summary>
+        /// raw-16khz-16bit-mono-pcm request output audio format type.
+        /// </summary>
+        Raw16Khz16BitMonoPcm,
+        /// <summary>
+        /// riff-8khz-8bit-mono-mulaw request output audio format type.
+        /// </summary>
+        Riff8Khz8BitMonoMULaw,
+        /// <summary>
+        /// riff-16khz-16bit-mono-pcm request output audio format type.
+        /// </summary>
+        Riff16Khz16BitMonoPcm,
+    }
+}
